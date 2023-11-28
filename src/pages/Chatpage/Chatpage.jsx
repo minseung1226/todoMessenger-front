eb0f32540f3cf39f81b43915cd063ecc9fbb9610
minseung1226 <minseung1226@naver.com>
2023-11-28 21:23:15 +0900

    채팅방 분리

--- src/pages/Chatpage/Chatpage.jsx
+++ src/pages/Chatpage/Chatpage.jsx
@@ -4,16 +4,25 @@ import {Button} from "@mui/base/Button"
 import MessageContainer from "../../components/MessageContainer/MessageContainer";
 import InputField from "../../components/InputField/InputField";
 import "./ChatPageStyle.css"
+import {useParams,useNavigate} from "react-router-dom"
 
 const ChatPage=({user})=>{
     const [messageList,setMessageList]=useState([]);
     const [message,setMessage]=useState("");
-
+    const {id}=useParams();
+    const navigate=useNavigate();
     useEffect(()=>{
         socket.on("message",(res)=>{
             console.log("message",res);
             setMessageList((prevState)=>prevState.concat(res));
         });
+        socket.emit("joinRoom",id,(res)=>{
+            if(res&& res.ok){
+                console.log("successfully join",res);
+            }else{
+                console.log("fail to join",res);
+            }
+        })
     },[]);
 
     const sendMessage=(event)=>{
@@ -26,21 +35,32 @@ const ChatPage=({user})=>{
         })
     }
 
+    const leaveRoom=()=>{
+        socket.emit("leaveRoom",user,(res)=>{
+            if(res.ok) navigate("/") 
+        })
+    }
+
     return(
         <div>
-            <div className="App">
-                <div>
-                    {messageList.length>0?(
-                        <MessageContainer messageList={messageList} user={user}/>
-                    ):null}
-                </div>
-                <InputField
-                    message={message}
-                    setMessage={setMessage}
-                    sendMessage={sendMessage}
-                    />
-            </div>
+        <div className="App">
+        {/* nav 이부분 추가  */}
+            <nav>
+              <Button onClick={leaveRoom} className='back-button'>←</Button>
+              <div className='nav-user'>{user.name}</div>
+            </nav>
+          <div>
+            {messageList.length > 0 ? (
+              <MessageContainer messageList={messageList} user={user} />
+            ) : null}
+          </div>
+          <InputField
+            message={message}
+            setMessage={setMessage}
+            sendMessage={sendMessage}
+          />
         </div>
+      </div>
     );
 }
 
