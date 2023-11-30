536efa04c9e8f61182fb1af1df565d958104ff4a
minseung1226 <minseung1226@naver.com>
2023-11-30 21:23:02 +0900

    Login page 개발

---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const LoginPage=()=>{
+
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <form
+        </div>
+    )
+}
