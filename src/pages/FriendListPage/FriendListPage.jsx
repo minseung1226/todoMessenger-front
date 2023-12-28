47803fefd6dc99108537572854368b7fda95bd26
minseung1226 <minseung1226@naver.com>
2023-12-29 05:06:22 +0900

    구조 변경 상위 컴포넌트에서 조회 후 하위 컴포넌트의 props로 전달

--- src/pages/FriendListPage/FriendListPage.jsx
+++ src/pages/FriendListPage/FriendListPage.jsx
@@ -3,25 +3,14 @@ import { useState,useEffect } from "react";
 import Logout from "../../components/Logout/Logout";
 import { useNavigate } from "react-router-dom";
 
-const FriendListPage=()=>{
+const FriendListPage=(friendList)=>{
     const server_url=process.env.REACT_APP_SERVER_URL;
     const token=sessionStorage.getItem("jwtToken");
     const navigate=useNavigate();
     const [friendInput,setFriendInput]=useState("");
-    const [friends,setFriends]=useState([]);
 
     useEffect(()=>{
-        const url=new URL(`${server_url}/friends`);
-        url.searchParams.append("friendName",friendInput);
-        fetch(url,{
-            method:"get",
-            headers:{
-                "Content-type":"application/json",
-                "Authorization":`Bearer ${token}`
-            }
-        }).then(res=>res.json())
-        .then(data=>setFriends(data.friends))
-        .catch(err=>console.log(err));
+
     },[friendInput])
 
     return (
@@ -29,12 +18,12 @@ const FriendListPage=()=>{
             <Input type="text" onChange={(event)=>setFriendInput(event.target.value)}/>
             <div>
                 <div >
-                    {friends?friends?.map((friend,index)=>(
+                    {friendList?friendList?.map((friend,index)=>(
                         <div key={index}> {friend.name}</div>
                     )):<div></div>}
                 </div>
             </div>
-            <Button type="button" onClick={()=>navigate("/rooms")}>채팅방이동</Button>           
+          
             <Logout/>
             <Button type="button" onClick={()=>navigate("/user/search")}>친구 추가</Button>
         </div>
--- src/pages/RootPage/RootPage.jsx
+++ src/pages/RootPage/RootPage.jsx
@@ -4,25 +4,48 @@ import { Container,Row,Col ,Nav} from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import RoomListPage from "../RoomListPage/RoomListPage";
 import FriendListPage from "../FriendListPage/FriendListPage";
+import "./RootPageStyle.css";
+import { getSocket } from "../../socket/socket";
 const RootPage=()=>{
     const [currentView,setCurrentView]=useState(CurrentView.friendList);
     const [friendList,setFriendList]=useState([]);
     const [roomList,setRoomList]=useState([]);
+    const socket=getSocket;
+    const token=sessionStorage.getItem("jwtToken");
+    useEffect(()=>{
+        socket.emit("friendList",token,(res)=>{
+            setFriendList(res);
+        })
+        // const url=new URL(`${server_url}/friends`);
+        // url.searchParams.append("friendName",friendInput);
+        // fetch(url,{
+        //     method:"get",
+        //     headers:{
+        //         "Content-type":"application/json",
+        //         "Authorization":`Bearer ${token}`
+        //     }
+        // }).then(res=>res.json())
+        // .then(data=>setFriendList(data.friends))
+        // .catch(err=>console.log(err));
+    },[friendList])
 
-
+    useEffect(()=>{
+        
+    },[roomList]);
     return(
         <Container fluid>
             <Row>
                 <Col md={2} className="sidebar">
-                    <Nav defaultActiveKey="/home" className="flex-column">
+                    <Nav className="flex-column">
                         <Nav.Link onClick={()=>setCurrentView(CurrentView.friendList)}>친구목록</Nav.Link>
                         <Nav.Link onClick={()=>setCurrentView(CurrentView.roomList)}>채팅방 목록</Nav.Link>
                     </Nav>  
                 </Col>
 
                 <Col md={10} className="content">
+                {currentView===CurrentView.friendList && <FriendListPage/>}
                     {currentView===CurrentView.roomList && <RoomListPage/>}
-                    {currentView===currentView.friendList && <FriendListPage/>}
+                    
                 </Col>
             </Row>
         </Container>
